78a23a4234a825248f01d252a64c1ba142e5088d
amartyajee <33002333+amartyajee@users.noreply.github.com>
2021-06-02 18:11:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import MyCard from "./myCard";
+import Grid from "@material-ui/core/Grid";
+import Draggable, { DraggableCore } from 'react-draggable';
 
 function App() {
+  const [data, setData] = useState([]);
+  const getData = () => {
+    fetch('data.json'
+      , {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      }
+    )
+      .then(function (response) {
+        console.log('response', response)
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(myJson);
+        setData(myJson)
+      });
+  }
+  useEffect(() => {
+    getData()
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <Grid container spacing={24}>
+        {
+          data.map((item) => <Draggable><Grid item md={4}><MyCard title={item.title} imageSrc={'/images/myImage_' + item.position + '.jpg'} /></Grid></Draggable>)
+        }
+      </Grid>
     </div>
   );
 }
